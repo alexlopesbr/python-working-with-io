8c9c3393a9f42da53bdb48838c8559562f476d75
alexlopesbr <criabiobr@gmail.com>
2023-01-12 20:39:41 -0300

    first commit

--- contact_utils.py
+++ contact_utils.py
@@ -3,32 +3,43 @@ import json
 
 from contact import Contact
 
+class Convert:
+    def __init__(self, path:str, contact=None, encoding:str=None):
+        self.path = path
+        self.contact = contact
+        self.encoding = encoding
 
-def csv_to_contact(path:str, encoding:str='latin_1') -> dict:
-    contacts = []
-    with open(path, 'r', encoding=encoding) as f:
-        reader = csv.reader(f)
-        for row in reader:
-            id, name, email = row
-            contact = Contact(id=id, name=name, email=email)
-            contacts.append(contact)
-    return contacts
+    def csv_to_contact(self) -> dict:
+        contacts = []
 
-def contacts_to_json(path:str, contacts):
-    with open(path, 'w', encoding='utf-8') as f:
-        json.dump(contacts, f, ensure_ascii=False, indent=4, default=_contact_representation)
+        with open(self.path, 'r', encoding=self.encoding) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                id, name, email = row
+                contact = Contact(id=id, name=name, email=email)
+                contacts.append(contact)
+        return contacts
 
-def _contact_representation(contact):
-    return contact.__dict__
+    def contacts_to_json(self):
+        with open(self.path, 'w', encoding='utf-8') as f:
+            json.dump(
+                self.contact,
+                f,
+                ensure_ascii=False,
+                indent=4,
+                default=self._contact_representation
+            )
 
-def json_for_contacts(path:str):
-    contacts = []
+    def json_for_contacts(self, test):
+        contacts = []
 
-    with open(path) as f:
-        contacts_in_json = json.load(f)
+        with open(self.path) as f:
+            contacts_in_json = json.load(f)
 
-        for contact in contacts_in_json:
-            # c = Contact(contact['id'], contact['name'], contact['email'])
-            c = Contact(**contact)
-            contacts.append(c)
-    return contacts
+            for contact in contacts_in_json:
+                c = Contact(**contact)
+                contacts.append(c)
+        return contacts
+
+    def _contact_representation(self, contact):
+        return contact.__dict__
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-import contact_utils
+# import contact_utils
+from contact_utils import Convert
 
 PATH_CSV = 'data/contacts.csv'
 PATH_JSON = 'data/contacts.json'
+PATH_JSON_2 = 'data/contacts_2.json'
 
 try:
-    # contacts = contact_utils.csv_to_contact(PATH_CSV, 'utf8')
-    # contact_json = contact_utils.contacts_to_json(PATH_JSON, contacts)
-    contacts_from_json = contact_utils.json_for_contacts(PATH_JSON)
+    json_for_contacts = Convert(PATH_JSON).json_for_contacts('test')
+    csv_to_contacts = Convert(path=PATH_CSV, encoding='utf8').csv_to_contact()
+    contacts_to_json = Convert(path=PATH_JSON_2, contact=json_for_contacts).contacts_to_json()
 except FileNotFoundError:
     print('File not found')
 except PermissionError:
     print('Permission denied')
 
-# for contact in contacts:
-#     print (f'{contact.id} - {contact.name} - {contact.email}')
-for contact in contacts_from_json:
-    print (f'{contact.id} - {contact.name} - {contact.email}')
+
+
+# for contact in csv_to_contacts:
+    # print (f'{contact.id} - {contact.name} - {contact.email}')
+
+# for contact in json_for_contacts:
+    # print (f'{contact.id} - {contact.name} - {contact.email}')
